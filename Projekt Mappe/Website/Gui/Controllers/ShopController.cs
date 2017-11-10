a328f3f3bb3a7fcca8706f9e9cd02042bc8288e6
tgilkrog <gilkroggaming@gmail.com>
2017-11-10 11:21:14 +0100

    Prøver websitebranchen

---
+++ Projekt Mappe/Website/Gui/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gui.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult shoppage()
+        {
+            return View();
+        }
+    }
+}
