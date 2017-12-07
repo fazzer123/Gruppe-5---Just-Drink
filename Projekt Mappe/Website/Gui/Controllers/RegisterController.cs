3d577f1b56f575a377854883c70ea7e1fb26104d
Farzad <fazzerking1@gmail.com>
2017-12-07 10:48:53 +0100

    Validator for email og phone number tilføjet når man opretter en bruger

--- Projekt Mappe/Website/Gui/Controllers/RegisterController.cs
+++ Projekt Mappe/Website/Gui/Controllers/RegisterController.cs
@@ -5,7 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Gui.UserServiceRef;
 using Gui.FavoritServiceRef;
-
+using Gui.Models;
+using System.Text.RegularExpressions;
 
 namespace Gui.Controllers
 {
@@ -13,9 +14,14 @@ namespace Gui.Controllers
     {
         UserServiceClient client = new UserServiceClient();
         FavoritesServiceClient faClient = new FavoritesServiceClient();
-    
+
         // GET: Register
         public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserModel email)
         {
             return View();
         }
@@ -29,13 +35,51 @@ namespace Gui.Controllers
 
         // POST: Register/Create
 
-            [HttpPost]
-            public ActionResult Create(Gui.UserServiceRef.User user)
+        [HttpPost]
+        public ActionResult Create(Gui.UserServiceRef.User user)
+        {
+            if (string.IsNullOrEmpty(user.UserName))
             {
-            client.CreateUser(user);
-            return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("Name", "Indtast username");
             }
+            if (!string.IsNullOrEmpty(user.Email))
+            {
+                string emailRegex = @"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$";
+                Regex re = new Regex(emailRegex);
+                if (!re.IsMatch(user.Email))
+                {
+                    ModelState.AddModelError("Email", "Indtast venligst korrekt email");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Email", "Indtast venligst email");
+            }
+            if (!string.IsNullOrEmpty(user.Phone))
+            {
+                string phoneRegex = "^([0-9]{8})$";
+                Regex pe = new Regex(phoneRegex);
+                if (!pe.IsMatch(user.Phone))
+                {
+                    ModelState.AddModelError("Phone", "Indtast venligst korrekt telefon nr");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Phone", "Indtast telefon nr");
+            }
+                if (ModelState.IsValid)
+                {
+
+                    client.CreateUser(user);
+                    return RedirectToAction("Create");
+                }
+                return View();
+            }
+
+
         }
 
-
     }
+
+    
